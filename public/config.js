e08dcaa867475287254a80584a5e55c0d10ee79f
Miguel Montesinos <MiguelMontesinos99@users.noreply.github.com>
2026-09-19 16:40:21 -0500

    Set initial Harvest administrator email

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 // Solo URL y publishable/anon key. Nunca coloques service_role aquí.
 window.HARVEST_SUPABASE_URL='https://npqadjqncmmkzwflveyr.supabase.co';
 window.HARVEST_SUPABASE_ANON_KEY='sb_publishable_pTH9sAVOl3Ikonlxa-VnyA_c6sN7Im0';
-window.HARVEST_SUPERADMIN_EMAIL='miguelfeic4@gmail.com';
+window.HARVEST_SUPERADMIN_EMAIL='miguelfeic5@gmail.com';
 window.HARVEST_WHATSAPP='51929535325';
